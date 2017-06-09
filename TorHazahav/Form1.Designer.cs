74fc643816ea1cdfb823884c4c9e9f839edafe35
Shiranma <Shiranma@SHIRANMA-LAP-HZ>
2017-06-09 11:02:18 +0300

    version2.2

--- TorHazahav/Form1.Designer.cs
+++ TorHazahav/Form1.Designer.cs
@@ -70,6 +70,7 @@
             this.strComments = new System.Windows.Forms.TextBox();
             this.label19 = new System.Windows.Forms.Label();
             this.label20 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // strId
@@ -493,11 +494,21 @@
             this.label20.TabIndex = 43;
             this.label20.Text = "שירן שירן";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(474, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 44;
+            this.button1.Text = "Eira";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1008, 561);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label20);
             this.Controls.Add(this.label19);
             this.Controls.Add(this.strComments);
@@ -592,6 +603,7 @@
         private System.Windows.Forms.TextBox strComments;
         private System.Windows.Forms.Label label19;
         private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Button button1;
     }
 }
 
