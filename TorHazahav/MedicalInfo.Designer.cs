5ddc4c046bc3a2b708c40a92a94e59ad46cef8bc
Shiranma <Shiranma@SHIRANMA-LAP-HZ>
2017-06-27 20:56:38 +0300

    shiran10.1

--- TorHazahav/MedicalInfo.Designer.cs
+++ TorHazahav/MedicalInfo.Designer.cs
@@ -33,8 +33,18 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.cityName = new System.Windows.Forms.ComboBox();
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.Hearing_chkbox = new System.Windows.Forms.CheckBox();
+            this.Vision__chkbox = new System.Windows.Forms.CheckBox();
+            this.Chronic__chkbox = new System.Windows.Forms.CheckBox();
+            this.Demensia__chkbox = new System.Windows.Forms.CheckBox();
+            this.Parkinson__chkbox = new System.Windows.Forms.CheckBox();
+            this.Other__chkbox = new System.Windows.Forms.CheckBox();
+            this.Severity_cbBox1 = new System.Windows.Forms.ComboBox();
+            this.Severity_cbBox2 = new System.Windows.Forms.ComboBox();
+            this.Severity_cbBox3 = new System.Windows.Forms.ComboBox();
+            this.Severity_cbBox4 = new System.Windows.Forms.ComboBox();
+            this.Severity_cbBox5 = new System.Windows.Forms.ComboBox();
+            this.Severity_cbBox6 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label2
@@ -51,7 +61,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.label3.Location = new System.Drawing.Point(290, 72);
+            this.label3.Location = new System.Drawing.Point(290, 61);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(50, 17);
             this.label3.TabIndex = 73;
@@ -61,7 +71,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.label1.Location = new System.Drawing.Point(290, 160);
+            this.label1.Location = new System.Drawing.Point(291, 260);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(49, 17);
             this.label1.TabIndex = 74;
@@ -71,7 +81,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.label4.Location = new System.Drawing.Point(256, 315);
+            this.label4.Location = new System.Drawing.Point(109, 61);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(83, 17);
             this.label4.TabIndex = 75;
@@ -79,41 +89,143 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(46, 182);
+            this.richTextBox1.Location = new System.Drawing.Point(68, 280);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(291, 113);
+            this.richTextBox1.Size = new System.Drawing.Size(268, 69);
             this.richTextBox1.TabIndex = 76;
             this.richTextBox1.Text = "";
             // 
-            // cityName
+            // Hearing_chkbox
             // 
-            this.cityName.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
-            this.cityName.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
-            this.cityName.FormattingEnabled = true;
-            this.cityName.ItemHeight = 14;
-            this.cityName.Items.AddRange(new object[] {
-            "רמת השרון"});
-            this.cityName.Location = new System.Drawing.Point(46, 315);
-            this.cityName.Name = "cityName";
-            this.cityName.Size = new System.Drawing.Size(199, 20);
-            this.cityName.TabIndex = 84;
+            this.Hearing_chkbox.AutoSize = true;
+            this.Hearing_chkbox.Location = new System.Drawing.Point(275, 89);
+            this.Hearing_chkbox.Name = "Hearing_chkbox";
+            this.Hearing_chkbox.Size = new System.Drawing.Size(61, 17);
+            this.Hearing_chkbox.TabIndex = 86;
+            this.Hearing_chkbox.Text = "שמיעה";
+            this.Hearing_chkbox.UseVisualStyleBackColor = true;
             // 
-            // checkedListBox1
+            // Vision__chkbox
             // 
-            this.checkedListBox1.BackColor = System.Drawing.Color.Gainsboro;
-            this.checkedListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Items.AddRange(new object[] {
-            "שמיעה",
-            "ראייה",
-            "מחלה כרונית",
-            "דמנציה",
-            "פרקינסון",
-            "אחר"});
-            this.checkedListBox1.Location = new System.Drawing.Point(46, 100);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(291, 45);
-            this.checkedListBox1.TabIndex = 85;
+            this.Vision__chkbox.AutoSize = true;
+            this.Vision__chkbox.Location = new System.Drawing.Point(279, 143);
+            this.Vision__chkbox.Name = "Vision__chkbox";
+            this.Vision__chkbox.Size = new System.Drawing.Size(57, 17);
+            this.Vision__chkbox.TabIndex = 87;
+            this.Vision__chkbox.Text = "ראייה";
+            this.Vision__chkbox.UseVisualStyleBackColor = true;
+            // 
+            // Chronic__chkbox
+            // 
+            this.Chronic__chkbox.AutoSize = true;
+            this.Chronic__chkbox.Location = new System.Drawing.Point(244, 116);
+            this.Chronic__chkbox.Name = "Chronic__chkbox";
+            this.Chronic__chkbox.Size = new System.Drawing.Size(92, 17);
+            this.Chronic__chkbox.TabIndex = 88;
+            this.Chronic__chkbox.Text = "מחלה כרונית";
+            this.Chronic__chkbox.UseVisualStyleBackColor = true;
+            // 
+            // Demensia__chkbox
+            // 
+            this.Demensia__chkbox.AutoSize = true;
+            this.Demensia__chkbox.Location = new System.Drawing.Point(272, 170);
+            this.Demensia__chkbox.Name = "Demensia__chkbox";
+            this.Demensia__chkbox.Size = new System.Drawing.Size(64, 17);
+            this.Demensia__chkbox.TabIndex = 89;
+            this.Demensia__chkbox.Text = "דמנציה";
+            this.Demensia__chkbox.UseVisualStyleBackColor = true;
+            // 
+            // Parkinson__chkbox
+            // 
+            this.Parkinson__chkbox.AutoSize = true;
+            this.Parkinson__chkbox.Location = new System.Drawing.Point(263, 197);
+            this.Parkinson__chkbox.Name = "Parkinson__chkbox";
+            this.Parkinson__chkbox.Size = new System.Drawing.Size(73, 17);
+            this.Parkinson__chkbox.TabIndex = 90;
+            this.Parkinson__chkbox.Text = "פרקינסון";
+            this.Parkinson__chkbox.UseVisualStyleBackColor = true;
+            // 
+            // Other__chkbox
+            // 
+            this.Other__chkbox.AutoSize = true;
+            this.Other__chkbox.Location = new System.Drawing.Point(289, 224);
+            this.Other__chkbox.Name = "Other__chkbox";
+            this.Other__chkbox.Size = new System.Drawing.Size(47, 17);
+            this.Other__chkbox.TabIndex = 91;
+            this.Other__chkbox.Text = "אחר";
+            this.Other__chkbox.UseVisualStyleBackColor = true;
+            // 
+            // Severity_cbBox1
+            // 
+            this.Severity_cbBox1.FormattingEnabled = true;
+            this.Severity_cbBox1.Items.AddRange(new object[] {
+            "קל",
+            "בינוני",
+            "קשה"});
+            this.Severity_cbBox1.Location = new System.Drawing.Point(68, 85);
+            this.Severity_cbBox1.Name = "Severity_cbBox1";
+            this.Severity_cbBox1.Size = new System.Drawing.Size(121, 21);
+            this.Severity_cbBox1.TabIndex = 92;
+            // 
+            // Severity_cbBox2
+            // 
+            this.Severity_cbBox2.FormattingEnabled = true;
+            this.Severity_cbBox2.Items.AddRange(new object[] {
+            "קל",
+            "בינוני",
+            "קשה"});
+            this.Severity_cbBox2.Location = new System.Drawing.Point(68, 112);
+            this.Severity_cbBox2.Name = "Severity_cbBox2";
+            this.Severity_cbBox2.Size = new System.Drawing.Size(121, 21);
+            this.Severity_cbBox2.TabIndex = 93;
+            // 
+            // Severity_cbBox3
+            // 
+            this.Severity_cbBox3.FormattingEnabled = true;
+            this.Severity_cbBox3.Items.AddRange(new object[] {
+            "קל",
+            "בינוני",
+            "קשה"});
+            this.Severity_cbBox3.Location = new System.Drawing.Point(68, 139);
+            this.Severity_cbBox3.Name = "Severity_cbBox3";
+            this.Severity_cbBox3.Size = new System.Drawing.Size(121, 21);
+            this.Severity_cbBox3.TabIndex = 94;
+            // 
+            // Severity_cbBox4
+            // 
+            this.Severity_cbBox4.FormattingEnabled = true;
+            this.Severity_cbBox4.Items.AddRange(new object[] {
+            "קל",
+            "בינוני",
+            "קשה"});
+            this.Severity_cbBox4.Location = new System.Drawing.Point(68, 166);
+            this.Severity_cbBox4.Name = "Severity_cbBox4";
+            this.Severity_cbBox4.Size = new System.Drawing.Size(121, 21);
+            this.Severity_cbBox4.TabIndex = 95;
+            // 
+            // Severity_cbBox5
+            // 
+            this.Severity_cbBox5.FormattingEnabled = true;
+            this.Severity_cbBox5.Items.AddRange(new object[] {
+            "קל",
+            "בינוני",
+            "קשה"});
+            this.Severity_cbBox5.Location = new System.Drawing.Point(68, 193);
+            this.Severity_cbBox5.Name = "Severity_cbBox5";
+            this.Severity_cbBox5.Size = new System.Drawing.Size(121, 21);
+            this.Severity_cbBox5.TabIndex = 96;
+            // 
+            // Severity_cbBox6
+            // 
+            this.Severity_cbBox6.FormattingEnabled = true;
+            this.Severity_cbBox6.Items.AddRange(new object[] {
+            "קל",
+            "בינוני",
+            "קשה"});
+            this.Severity_cbBox6.Location = new System.Drawing.Point(68, 220);
+            this.Severity_cbBox6.Name = "Severity_cbBox6";
+            this.Severity_cbBox6.Size = new System.Drawing.Size(121, 21);
+            this.Severity_cbBox6.TabIndex = 97;
             // 
             // MedicalInfo
             // 
@@ -121,8 +233,18 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gainsboro;
             this.ClientSize = new System.Drawing.Size(404, 381);
-            this.Controls.Add(this.checkedListBox1);
-            this.Controls.Add(this.cityName);
+            this.Controls.Add(this.Severity_cbBox6);
+            this.Controls.Add(this.Severity_cbBox5);
+            this.Controls.Add(this.Severity_cbBox4);
+            this.Controls.Add(this.Severity_cbBox3);
+            this.Controls.Add(this.Severity_cbBox2);
+            this.Controls.Add(this.Severity_cbBox1);
+            this.Controls.Add(this.Other__chkbox);
+            this.Controls.Add(this.Parkinson__chkbox);
+            this.Controls.Add(this.Demensia__chkbox);
+            this.Controls.Add(this.Chronic__chkbox);
+            this.Controls.Add(this.Vision__chkbox);
+            this.Controls.Add(this.Hearing_chkbox);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
@@ -144,7 +266,17 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.ComboBox cityName;
-        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckBox Hearing_chkbox;
+        private System.Windows.Forms.CheckBox Vision__chkbox;
+        private System.Windows.Forms.CheckBox Chronic__chkbox;
+        private System.Windows.Forms.CheckBox Demensia__chkbox;
+        private System.Windows.Forms.CheckBox Parkinson__chkbox;
+        private System.Windows.Forms.CheckBox Other__chkbox;
+        private System.Windows.Forms.ComboBox Severity_cbBox1;
+        private System.Windows.Forms.ComboBox Severity_cbBox2;
+        private System.Windows.Forms.ComboBox Severity_cbBox3;
+        private System.Windows.Forms.ComboBox Severity_cbBox4;
+        private System.Windows.Forms.ComboBox Severity_cbBox5;
+        private System.Windows.Forms.ComboBox Severity_cbBox6;
     }
 }
